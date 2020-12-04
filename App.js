88bca65663304886969b2c78011c23ea5a3e260a
cotton <chinoknct@gmail.com>
2020-12-05 01:37:03 +0900

    [add]環境構築

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import React, { useReducer, useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Top } from "./pages/Top";
+
+const Stack = createStackNavigator();
+
+const CigarettesNumContext = React.createContext(null);
 
 export default function App() {
+  // const [state, dispatch] = useReducer(action, []);
+  // const value = { state, dispatch };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <TaskContext.Provider value={value}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HOME"
+          component={ Top }
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </TaskContext.Provider>
   );
 }
 
@@ -19,3 +38,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export {
+  CigarettesNumContext
+}
---
+++ pages/Top.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { ScrollView, Image, TextInput, StyleSheet, Text, View, Button, TouchableOpacity, Keyboard } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { CigarettesNumContext } from "../App";
+
+const Top = ({ navigation }) => {
+  const [tasks, setTasks] = useState([]);
+  // const { state, dispatch } = useContext(CigarettesNumContext);
+
+  useEffect(() => {}, []);
+
+  const getData = async () => {};
+
+  const deleteTask = async (id) => {};
+
+  const Header = () => {
+    return (
+      <TouchableOpacity
+        style={styles.header_wrapper}
+        onPress={() => navigation.navigate("HOME")}
+      >
+        <Text style={styles.header_text}>ADD</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <ScrollView style={styles.scroll_container}>
+      </ScrollView>
+    </View>
+  );
+}
+
+export {
+  Top
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    paddingTop: 10,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: "center"
+  },
+  scroll_container: {
+    width: "90%",
+  },
+  header_wrapper: {
+    width: "100%",
+    height: 100,
+    borderBottomColor: "gray",
+    borderBottomWidth: 2,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+  header_text: {
+    width: "20%",
+    height: 70,
+    lineHeight: 70,
+    fontSize: 20,
+    textAlign: "center",
+  },
+  task_wrapper: {
+    width: "100%",
+    height: 60,
+    borderBottomColor: "gray",
+    borderBottomWidth: 2,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  task_text: {
+    fontSize: 20,
+    lineHeight: 60,
+  },
+  check_wrapper: {
+    height: 58,
+    width: "15%",
+  },
+  check_box: {
+    height: 58,
+    width: "100%",
+    backgroundColor: "red"
+  },
+  check_img: {
+    margin: 9,
+    height: 40,
+    width: 40
+  }
+});
